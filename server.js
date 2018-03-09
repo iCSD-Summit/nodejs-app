5b807b812c95aa0d43d5717248e318c1bc765a84
M.Suska <msuska@gmail.com>
2018-03-09 10:02:20 +0100

    Added push endpoint

--- server.js
+++ server.js
@@ -31,6 +31,12 @@ app.get('/say-hello', function (req, res) {
 app.use(fallback('index.html', {root: root}));
 
 // PUSH NOTIFICATION
+app.post('/push/:title/:msg', (req, res) => {
+    var title = req.params.title,
+        msg = req.params.msg;
+    push(title, msg);
+    res.send(`Sent ${title}: ${msg}.`);
+});
 
 // SUBSCRIBERS
 app.post('/registerSubscription', (req, res) => { 
@@ -62,6 +68,7 @@ app.post('/unregisterSubscription', (req, res) => {
     });
 });
 
+// PRIVATE METHODS
 function getAllSubscriptions() {
     return new Promise((resolve, reject) => { 
         file.readFile(subscriptionsFile, (err, data) => {
@@ -90,58 +97,25 @@ function saveSubscription(subscriptions) {
     });
 }
 
+function push(title, msg) {
+    getAllSubscriptions().then((subscriptions) => {
+        subscriptions.forEach(subscriber => {
+        console.log(`Sending to ${subscriber.endpoint}...`);
+        webpush.sendNotification(
+            subscriber,
+            JSON.stringify({title: title,
+                            body: msg,
+                            icon: '/images/coffee.png',
+                            badge: '/images/coffee-beans.png',
+                            vibrate: [200, 100, 200, 100, 200, 100, 200]
+                            }),
+            options).catch((err) => {
+                console.log('Error while pushing to [' + subscriber.endpoint + ']: ' + err.statusCode + ', ' + err.body);
+            });
+        });
+    });
+}
 
-// const webpush = require('web-push'),
-//       bodyParser = require('body-parser'),
-//       nodeSchedule = require('node-schedule'),
-//       options = {
-//           vapidDetails: {
-//               subject: 'http://www.google.com',
-//               publicKey: 'BPOOvDThOUuSxiC-DuMv-WXG0XbSCwKR6Jux0CkyeyZ86OMF2U64VYKksNCJKoJdq1ISzYKfCUUxB6hKM0zeNGA',
-//               privateKey: 'UGU2kXUSbtRVFxDuqNpbSVfzU3C6RrRznJXi3e6G_tM'
-//           },
-//           TTL: 60 * 60
-//       };
-//
-// var subscriptions = [];
-//
-// app.use(bodyParser.json());
-//
-// app.post('/push/:title/:msg', (req, res) => {
-//     var title = req.params.title,
-//         msg = req.params.msg;
-//     push(title, msg);
-//     res.send(`Sent ${title}: ${msg}.`);
-// });
-//
-//
-// function push(title, msg) {
-//     subscriptions.forEach(subscriber => {
-//         console.log(`Sending to ${subscriber.endpoint}...`);
-//         webpush.sendNotification(
-//             subscriber,
-//             JSON.stringify({title: title,
-//                             body: msg,
-//                             icon: '/images/coffee.png',
-//                             badge: '/images/coffee-beans.png',
-//                             vibrate: [200, 100, 200, 100, 200, 100, 200]
-//                            }),
-//             options).catch((err) => {
-//                 console.log('Error while pushing to [' + subscriber.endpoint + ']: ' + err.statusCode + ', ' + err.body);
-//             });
-//     });
-// }
-//
-
-//
-// app.post('/unregisterSubscription', (req, res) => {
-//     var subscriptionObject = req.body.subscription;
-//
-//     subscriptions = subscriptions.filter(el =>
-//         el.endpoint !== subscriptionObject.endpoint);
-//     res.status(200).send({success: true});
-// });
-//
 // function schedule(day, h, m, type) {
 //     nodeSchedule.scheduleJob(new Date(2017, 2, day, h, m, 0), () => {
 //         console.log(`Sending ${type}`);
